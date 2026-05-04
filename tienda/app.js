e4343b9cd570cf1851ae73099a1f2501ed072325
palmaspizza <75686833+palmaspizza@users.noreply.github.com>
2026-05-04 01:53:26 -0400

    Update app.js

--- tienda/app.js
+++ tienda/app.js
@@ -5,7 +5,7 @@
 
 // ==================== FIREBASE ====================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, collection, getDocs, query, orderBy, limit, startAfter }
+import { getFirestore, collection, getDocs, query, orderBy, limit, startAfter } 
   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyCRJ_l7BXJtwVDGc-KbBkLuSgMCBDtGY4M",
